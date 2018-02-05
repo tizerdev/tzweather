b979a8300daf0495bb18a5c7d8b68ded41b38bfd
Jocker <Jocker@gmail.com>
2018-02-05 21:05:25 +0800

    加入数据库和表的各项配置

---
+++ app/src/main/java/com/tzweather/android/db/County.java
@@ -0,0 +1,46 @@
+package com.tzweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 桃子 on 2018/2/5.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
---
+++ app/src/main/java/com/tzweather/android/db/Province.java
@@ -0,0 +1,37 @@
+package com.tzweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 桃子 on 2018/2/5.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
